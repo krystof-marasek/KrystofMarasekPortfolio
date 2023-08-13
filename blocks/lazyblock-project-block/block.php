bafca5504b5e0b3e84dd67fc2dc021b907175203
krys0381 <krys0381@easv365.dk>
2023-08-13 19:27:19 +0200

    Adding additional animations

--- blocks/lazyblock-project-block/block.php
+++ blocks/lazyblock-project-block/block.php
@@ -12,13 +12,13 @@
     	  	      	  	<p>
                             <?php echo $project["description"]?>
                         </p>
-    	  	      	  	<div class="secondary-block-text-button col-12 d-flex justify-content-start align-item-center">
+    	  	      	  	<div class="secondary-block-text-button col-12 d-flex justify-content-start align-item-center" data-aos="zoom-in-right">
     		    			<a href="<?php echo $project["button-link"]?>"><button type="button" class="btn btn-primary" style="margin:0;"><?php echo $project["button-text"]?></button></a>
     	  	      	  	</div>
     	  	      	</div>
     	  	    </div>
                 <div class="secondary-block-video col-lg-6 col-12 d-flex align-items-center">
-                    <div class="showcase-video">
+                    <div class="showcase-video" data-aos="fade-left" data-aos-duration="750">
                         <iframe width="560" height="315" src="<?php echo $project["video-link"]?>" title="<?php echo $project["video-title"]?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
     	  	    </div>  
